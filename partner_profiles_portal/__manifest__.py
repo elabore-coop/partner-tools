5c16620fe6746d49f1614df488893a3934e164ea
Stéphan Sainléger <contact@stephansainleger.fr>
2023-04-18 18:10:06 +0200

    [IMP] partner_profiles_portal: refactoring of user information
    Merge the main and public information edition in /my/account portal
    page.

--- partner_profiles_portal/__manifest__.py
+++ partner_profiles_portal/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "partner_profiles_portal",
-    "version": "12.0.1.0.2",
+    "version": "12.0.2.0.0",
     "author": "Elabore",
     "website": "https://elabore.coop",
     "maintainer": "Stéphan Sainléger",
@@ -28,6 +28,7 @@
         "views/portal_home_template.xml",
         "views/portal_my_profiles_template.xml",
         "views/portal_partner_profile_template.xml",
+        "views/portal_my_account.xml",
         "views/res_partner_view.xml",
     ],
     # only loaded in demonstration mode
