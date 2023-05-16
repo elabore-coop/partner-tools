7fbbb4f396ef5fc5aabae8014a4510b506ff4ccf
Stéphan Sainléger <contact@stephansainleger.fr>
2023-05-16 14:57:55 +0200

    [I18N] partner_profiles_portal: update french translations

--- partner_profiles_portal/__manifest__.py
+++ partner_profiles_portal/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "partner_profiles_portal",
-    "version": "12.0.2.2.1",
+    "version": "12.0.2.2.2",
     "author": "Elabore",
     "website": "https://elabore.coop",
     "maintainer": "Stéphan Sainléger",
