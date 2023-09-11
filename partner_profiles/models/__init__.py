e03fe31c864678ec9ac46990e8d005a74adae568
clementthomas <clement@cotyl.net>
2023-09-11 17:39:11 +0200

    remove modules in v12

--- partner_profiles/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2020 Elabore (https://elabore.coop)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import partner_profile
-from . import res_partner
--- partner_profiles_portal/controllers/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import portal_my_structures
-from . import portal_structure_profile
-from . import portal_my_positions
-from . import portal_position_profile
-from . import portal_my_account
