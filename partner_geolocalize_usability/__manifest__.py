ac95e1537c1f0767b7e6c4fd52c6084f47e477df
Stéphan Sainléger <contact@stephansainleger.fr>
2023-09-12 15:50:48 +0200

    [NEW] partner_geolocalize_usability: create add-on

---
+++ partner_geolocalize_usability/__manifest__.py
@@ -0,0 +1,35 @@
+# Copyright 2023 Stéphan Sainléger (Elabore)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "partner_geolocalize_usability",
+    "version": "12.0.1.0.0",
+    "author": "Elabore",
+    "website": "https://elabore.coop",
+    "maintainer": "Stéphan Sainléger",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "summary": "Brings several enhancements on geolocalize functionnalities",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "base",
+        "base_geolocalize"
+    ],
+    "qweb": [],
+    "external_dependencies": {
+        "python": [],
+    },
+    # always loaded
+    "data": [
+        "views/res_partner.xml",
+    ],
+    # only loaded in demonstration mode
+    "demo": [],
+    "js": [],
+    "css": [],
+    "installable": True,
+    # Install this module automatically if all dependency have been previously
+    # and independently installed.  Used for synergetic or glue modules.
+    "auto_install": False,
+    "application": False,
+}
