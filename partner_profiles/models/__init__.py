df455303fa6214721e515c3f9d879fdc9a277ebc
Boris Gallet <boris.gallet@gmail.com>
2024-03-06 11:35:02 +0100

    [MIG] partner_profiles: Migration to 16.0

---
+++ partner_profiles/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2020 Elabore (https://elabore.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import partner_profile
+from . import res_partner
---
+++ partner_profiles/wizard/__init__.py
@@ -0,0 +1,2 @@
+
+from . import create_position_profile
