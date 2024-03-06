6336ba1011db0ce6ce08b5306cbc7648e9752d12
Boris Gallet <boris.gallet@gmail.com>
2024-03-06 14:55:35 +0100

    [MIG] partner_profiles_geolocalize_usability: Migration to 16.0

---
+++ partner_profiles_geolocalize_usability/__manifest__.py
@@ -0,0 +1,38 @@
+# Copyright 2023 Stéphan Sainléger (Elabore)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "partner_profiles_geolocalize_usability",
+    "version": "16.0.1.0.0",
+    "author": "Elabore",
+    "website": "https://elabore.coop",
+    "maintainer": "Stéphan Sainléger",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "summary": "Adapt geolocalize usability to partner profiles logic",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "base",
+        "partner_geolocalize_usability",
+        "partner_profiles",
+    ],
+    "qweb": [
+        # "static/src/xml/*.xml",
+    ],
+    "external_dependencies": {
+        "python": [],
+    },
+    # always loaded
+    "data": [
+        "views/res_partner.xml",
+    ],
+    # only loaded in demonstration mode
+    "demo": [],
+    "js": [],
+    "css": [],
+    "installable": True,
+    # Install this module automatically if all dependency have been previously
+    # and independently installed.  Used for synergetic or glue modules.
+    "auto_install": False,
+    "application": False,
+}
---
+++ partner_profiles_geolocalize_usability/models/res_partner.py
@@ -0,0 +1,22 @@
+from odoo import models, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    def sync_admin_and_public_data(self):
+        super(ResPartner, self).sync_admin_and_public_data()
+        for partner in self:
+            if partner.is_main_profile and partner.public_profile_id:
+                main_partner = partner
+                public_partner = partner.public_profile_id
+            elif partner.is_public_profile and partner.contact_id:
+                main_partner = partner.contact_id
+                public_partner = partner
+
+            values = {
+                "manual_geolocate": main_partner.manual_geolocate,
+                "partner_latitude": main_partner.partner_latitude,
+                "partner_longitude": main_partner.partner_longitude,
+            }
+            public_partner.write(values)
