e8e08c466ea4672ad1d2de89754029151746cf8c
Boris Gallet <boris.gallet@gmail.com>
2024-03-06 09:58:04 +0100

    [MIG] partner_geolocalize_usability: Migration to 16.0

---
+++ partner_geolocalize_usability/models/res_partner.py
@@ -0,0 +1,11 @@
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    manual_geolocate = fields.Boolean('Geolocate yourself')
+
+    def geo_localize(self):
+        partners = self.filtered(lambda a: a.manual_geolocate == False)
+        return super(ResPartner, partners).geo_localize()
