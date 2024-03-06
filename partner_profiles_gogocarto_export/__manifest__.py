d282af1d446d72d15c7a4a7c46f4b75b37ea098e
Boris Gallet <boris.gallet@gmail.com>
2024-03-06 15:43:34 +0100

    [MIG] partner_profiles_gogocarto_export : Migration to 16.0

---
+++ partner_profiles_gogocarto_export/__manifest__.py
@@ -0,0 +1,40 @@
+# Copyright 2023 Stéphan Sainléger (Elabore)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "partner_profiles_gogocarto_export",
+    "version": "16.0.1.0.0",
+    "author": "Elabore",
+    "website": "https://elabore.coop",
+    "maintainer": "Stéphan Sainléger",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "summary": "Adapt data export for gogocarto to partner profiles logic",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "base",
+        "partner_gogocarto_export_api",
+        "partner_profiles",
+        "partner_profiles_geolocalize_usability",
+    ],
+    "qweb": [
+        # "static/src/xml/*.xml",
+    ],
+    "external_dependencies": {
+        "python": [],
+    },
+    # always loaded
+    "data": [
+        "views/config_settings_view.xml",
+        "views/res_company_view.xml",
+    ],
+    # only loaded in demonstration mode
+    "demo": [],
+    "js": [],
+    "css": [],
+    "installable": True,
+    # Install this module automatically if all dependency have been previously
+    # and independently installed.  Used for synergetic or glue modules.
+    "auto_install": False,
+    "application": False,
+}
