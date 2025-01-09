423262d1906f98fd6a41a4b4fe176da536de4938
Stéphan Sainléger <contact@stephansainleger.fr>
2025-01-09 14:06:10 +0100

    [NEW] partner_complementary_data_view: create add-on

---
+++ partner_complementary_data_view/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Complementary Contact Data View",
+    "summary": "Add extra_contact_ids in dedicated notebook page",
+    "version": "16.0.1.0.0",
+    "author": "Stéphan Sainléger",
+    "license": "AGPL-3",
+    "maintainer": "Elabore",
+    "category": "Partner Tools",
+    "website": "https://odoo-community.org/",
+    "depends": ["complementary_contact_data"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+}
