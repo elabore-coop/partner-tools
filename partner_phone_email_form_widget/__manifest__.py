4642927ce076cd87c51061c0cb65df38e1b87166
Stéphan Sainléger <contact@stephansainleger.fr>
2025-03-23 11:59:08 +0100

    [NEW] partner_phone_email_form_widget: create add-on

---
+++ partner_phone_email_form_widget/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Phone and Email form widget",
+    "summary": "Add widget button for phone and email fields in partner form view",
+    "version": "16.0.1.0.0",
+    "author": "Stéphan Sainléger",
+    "license": "AGPL-3",
+    "maintainer": "Elabore",
+    "category": "Partner Tools",
+    "website": "https://odoo-community.org/",
+    "depends": ["contacts"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+}
